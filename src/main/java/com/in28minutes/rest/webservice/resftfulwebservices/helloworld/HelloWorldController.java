73055ec45609963a75dd201a04b7ecf7f85fb8d8
ManuelaBarth <manubarth.jippie8390@gmail.com>
2024-04-18 15:16:00 +0200

    internationalized the hello world service

--- src/main/java/com/in28minutes/rest/webservice/resftfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservice/resftfulwebservices/helloworld/HelloWorldController.java
@@ -1,9 +1,20 @@
 package com.in28minutes.rest.webservice.resftfulwebservices.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World";
@@ -13,9 +24,16 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
     }
+
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternatationalized() {
+
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+    }
 }
